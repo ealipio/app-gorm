fe95470867026787ea0d53d8fadb7b27583b4d8d
Eisson Alipio <eisson.alipio@gmail.com>
2019-04-09 10:33:10 -0500

    restore main

--- main.go
+++ main.go
@@ -1,42 +1,40 @@
 package main
 
 import (
-	// "github.com/jinzhu/gorm"
-	// _ "github.com/jinzhu/gorm/dialects/sqlite"
-	ct "github.com/ealipio/app-with-gorm/context"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 // Product vdfbdfb
-// type Product struct {
-// 	gorm.Model
-// 	Code  string
-// 	Price uint
-// }
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
 
 func main() {
-	/*
-		db, err := gorm.Open("sqlite3", "test.db")
-		if err != nil {
-			panic("failed to connect database")
-		}
-		defer db.Close()
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
 
-		// Migrate the schema
-		db.AutoMigrate(&Product{})
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
 
-		// Create
-		db.Create(&Product{Code: "L1212", Price: 1000})
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
 
-		// Read
-		var product Product
-		db.First(&product, 1)                   // find product with id 1
-		db.First(&product, "code = ?", "L1212") // find product with code l1212
-		fmt.Println(product)
-		// Update - update product's price to 2000
-		db.Model(&product).Update("Price", 2000)
+	// Read
+	var product Product
+	db.First(&product, 1)                   // find product with id 1
+	db.First(&product, "code = ?", "L1212") // find product with code l1212
+	fmt.Println(product)
+	// Update - update product's price to 2000
+	db.Model(&product).Update("Price", 2000)
 
-		// Delete - delete product
-		db.Delete(&product)
-	*/
-	ct.StartUp()
+	// Delete - delete product
+	db.Delete(&product)
 }
