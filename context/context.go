62c2a3beeb2014ffaf3dd70fa37a5a6391abd94a
Eisson Alipio <eisson.alipio@gmail.com>
2019-04-09 10:18:31 -0500

    feat: using select for context

---
+++ context/context.go
@@ -0,0 +1,24 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// StartUp the code here
+func StartUp() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	mySleepAndTalk(ctx, 5*time.Second, "Hello")
+}
+
+func mySleepAndTalk(ctx context.Context, d time.Duration, msg string) {
+	select {
+	case <-time.After(d):
+		fmt.Println(msg)
+		case <- ctx.Done()
+		log.Print(ctx.Err())
+	}
+}
